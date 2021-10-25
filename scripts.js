b7eadbb3fed02a75a312d0d39f80f8eacebd6584
Ash Sundaram <ashwinrsundaram@gmail.com>
2021-10-26 00:04:56 +1300

    Created dynamic slider to draw grid based on user input

--- scripts.js
+++ scripts.js
@@ -2,24 +2,30 @@ const canvas = document.querySelector('#canvas');
 console.log(canvas);
 const canvasWidth = canvas.offsetWidth;
 console.log(canvasWidth);
+const slider = document.getElementById('grid-slider');
+console.log(slider);
+slider.addEventListener('input',drawGrid);
+console.log(slider.value);
 
 function calcDimensions(numOfRows){
     let width = canvasWidth/numOfRows;
     return width;
 }
 
-
-
-function drawGrid(){
-    let width = calcDimensions(16);
-    
-    for(let i=0; i<256; i++){
+function drawGrid(e){
+    canvas.innerHTML='';
+    let rowCount = e.target.value;
+    let cellWidth = calcDimensions(rowCount);
+    let gridSize = rowCount * rowCount;
+    console.log(rowCount);
+    console.log(cellWidth);
+    console.log(gridSize);
+    for(let i=0; i<gridSize; i++){
         const gridDiv = document.createElement('div');
     gridDiv.classList.add('grid-square');
-    gridDiv.style.width='31.25px';
-    gridDiv.style.height='31.25px';
+    gridDiv.style.width = `${cellWidth}px`;
+    gridDiv.style.height = `${cellWidth}px`;
         canvas.appendChild(gridDiv);
     }
 }
 
-drawGrid();
