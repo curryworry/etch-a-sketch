19e7cc69e34abe4cbeb5bf5caa7883b29228336d
Ash Sundaram <ashwinrsundaram@gmail.com>
2021-10-26 16:01:18 +1300

    Created sketch functions that allows user to paint on hover

--- scripts.js
+++ scripts.js
@@ -1,25 +1,27 @@
 const canvas = document.querySelector('#canvas');
-console.log(canvas);
 const canvasWidth = canvas.offsetWidth;
-console.log(canvasWidth);
 const slider = document.getElementById('grid-slider');
-console.log(slider);
-slider.addEventListener('input',drawGrid);
-console.log(slider.value);
+initializeGrid();
+slider.addEventListener('input',redrawGrid);
+//canvas.addEventListener('mouseover',sketch);
+
+
+function sketch(e){
+    console.log(e);
+    e.target.style.backgroundColor='black';
+}
+
 
 function calcDimensions(numOfRows){
     let width = canvasWidth/numOfRows;
     return width;
 }
 
-function drawGrid(e){
+function initializeGrid(){
     canvas.innerHTML='';
-    let rowCount = e.target.value;
+    let rowCount = slider.value;
     let cellWidth = calcDimensions(rowCount);
     let gridSize = rowCount * rowCount;
-    console.log(rowCount);
-    console.log(cellWidth);
-    console.log(gridSize);
     for(let i=0; i<gridSize; i++){
         const gridDiv = document.createElement('div');
     gridDiv.classList.add('grid-square');
@@ -27,5 +29,25 @@ function drawGrid(e){
     gridDiv.style.height = `${cellWidth}px`;
         canvas.appendChild(gridDiv);
     }
+    document.getElementById('grid-size-value').textContent = `${rowCount} x ${rowCount}`;
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach((square)=>square.addEventListener('mouseover',sketch));
+}
+
+function redrawGrid(e){
+    canvas.innerHTML='';
+    let rowCount = e.target.value;
+    let cellWidth = calcDimensions(rowCount);
+    let gridSize = rowCount * rowCount;
+    for(let i=0; i<gridSize; i++){
+        const gridDiv = document.createElement('div');
+    gridDiv.classList.add('grid-square');
+    gridDiv.style.width = `${cellWidth}px`;
+    gridDiv.style.height = `${cellWidth}px`;
+        canvas.appendChild(gridDiv);
+    }
+    document.getElementById('grid-size-value').textContent = `${rowCount} x ${rowCount}`;
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach((square)=>square.addEventListener('mouseover',sketch));
 }
 
