4002f3b33e3acfe30202a0b3edc84096700b7bde
Ash Sundaram <ashwinrsundaram@gmail.com>
2021-10-26 18:49:26 +1300

    Refactored JS to remove initializeGrid function, and combine this into drawGrid.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
 const canvas = document.querySelector('#canvas');
 const canvasWidth = canvas.offsetWidth;
 const slider = document.getElementById('grid-slider');
-initializeGrid();
-slider.addEventListener('input',redrawGrid);
-//canvas.addEventListener('mouseover',sketch);
-
-
-function sketch(e){
-    console.log(e);
-    e.target.style.backgroundColor='black';
-}
+const clearButton = document.getElementById('clr-btn');
+drawGrid();
+slider.addEventListener('input',drawGrid);
+clearButton.addEventListener('click',clearGrid);
 
 
 function calcDimensions(numOfRows){
@@ -17,9 +12,17 @@ function calcDimensions(numOfRows){
     return width;
 }
 
-function initializeGrid(){
+function drawGrid(e){
     canvas.innerHTML='';
-    let rowCount = slider.value;
+    let rowCount;
+    console.log(e);
+    if(e){
+    rowCount = e.target.value;
+    }
+    else{
+    rowCount = slider.value;
+    }
+    
     let cellWidth = calcDimensions(rowCount);
     let gridSize = rowCount * rowCount;
     for(let i=0; i<gridSize; i++){
@@ -34,20 +37,13 @@ function initializeGrid(){
     gridSquares.forEach((square)=>square.addEventListener('mouseover',sketch));
 }
 
-function redrawGrid(e){
+function sketch(e){
+    console.log(e);
+    e.target.style.backgroundColor='black';
+}
+
+function clearGrid(){
     canvas.innerHTML='';
-    let rowCount = e.target.value;
-    let cellWidth = calcDimensions(rowCount);
-    let gridSize = rowCount * rowCount;
-    for(let i=0; i<gridSize; i++){
-        const gridDiv = document.createElement('div');
-    gridDiv.classList.add('grid-square');
-    gridDiv.style.width = `${cellWidth}px`;
-    gridDiv.style.height = `${cellWidth}px`;
-        canvas.appendChild(gridDiv);
-    }
-    document.getElementById('grid-size-value').textContent = `${rowCount} x ${rowCount}`;
-    const gridSquares = document.querySelectorAll('.grid-square');
-    gridSquares.forEach((square)=>square.addEventListener('mouseover',sketch));
+    initializeGrid();
 }
 
