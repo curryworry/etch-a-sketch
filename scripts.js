a858c9043d3a00515a7f471ed4e8d5c92ab4498b
Ash Sundaram <ashwinrsundaram@gmail.com>
2021-10-27 15:27:06 +1300

    Added Random Mode that allows users to paint with a brush that generates random colors per cell

--- scripts.js
+++ scripts.js
@@ -3,20 +3,36 @@ const canvasWidth = canvas.offsetWidth;
 const slider = document.getElementById('grid-slider');
 const clearButton = document.getElementById('clr-btn');
 const colorPicker = document.getElementById('color-picker');
-let sketchColor = colorPicker.value;
+const randomButton = document.getElementById('random-btn');
+let brushColor = colorPicker.value;
+let paintMode = 'single';
 
 drawGrid();
 slider.addEventListener('input',drawGrid);
 clearButton.addEventListener('click',clearGrid);
-colorPicker.addEventListener('input',chooseSketchColor);
+colorPicker.addEventListener('input',chooseBrushColor);
+randomButton.addEventListener('click',chooseMode);
+colorPicker.addEventListener('click',chooseMode);
+
+function chooseMode(e){
+    console.log(e);
+    if(e.target.id=="random-btn"){
+        paintMode = 'random';
+        console.log(paintMode);
+    }
+    else if(e.target.id=="color-picker"){
+        paintMode = 'single';
+        console.log(paintMode);
+    }
+}
 
 function calcDimensions(numOfRows){
     let width = canvasWidth/numOfRows;
     return width;
 }
 
-function chooseSketchColor(e){
-    sketchColor = e.target.value;
+function chooseBrushColor(e){
+    brushColor = e.target.value;
 }
 
 function drawGrid(e){
@@ -45,11 +61,34 @@ function drawGrid(e){
 }
 
 function sketch(e){
-    e.target.style.backgroundColor=sketchColor;
+    /*let brushRgbColor = hexToRgb(brushColor);
+    let currentCellColor = e.target.style.backgroundColor;
+    let rgbCellColor = currentCellColor.match(/\d+/g);
+    if(rgbCellColor){console.log(`Current cell color in RGB is ${rgbCellColor[0]}`);}
+    console.log(`Current Cell Color is ${currentCellColor}`);
+    console.log(`Brush color is ${brushColor}`);*/
+    let paintR = Math.floor(Math.random() * 255) + 1;
+    let paintG = Math.floor(Math.random() * 255) + 1;
+    let paintB = Math.floor(Math.random() * 255) + 1;
+    if(paintMode=='single'){
+    e.target.style.backgroundColor=brushColor;
+    }
+    else if(paintMode=='random'){
+        console.log(`rgb(${paintR},${[paintG]},${paintB})`);
+        e.target.style.backgroundColor=`rgb(${paintR},${[paintG]},${paintB})`;
+    }
 }
 
 function clearGrid(){
     canvas.innerHTML='';
-    initializeGrid();
+    drawGrid();
 }
 
+function hexToRgb(hex) {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        let r= parseInt(result[1], 16);
+        let g= parseInt(result[2], 16);
+        let b= parseInt(result[3], 16);
+        console.log(`rgb(${r},${g},${b})`);
+        return `rgb(${r},${g},${b})`;
+}
